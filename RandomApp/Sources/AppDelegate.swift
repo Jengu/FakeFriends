fed508033a52db9e50584eb2cae4832b89a21ea8
Ilyas Siraev <isiraev@spaceotechnologies.ru>
2016-09-16 18:41:45 +0700

    Add initial project

---
+++ RandomApp/Sources/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  RandomApp
+//
+//  Created by Ilyas Siraev on 16.09.16.
+//  Copyright © 2016 On The Moon. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+
+}
